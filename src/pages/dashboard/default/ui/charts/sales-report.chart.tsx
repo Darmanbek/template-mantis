0e92f85ae8b7f491f2f6499cf14f79411903df69
Darmanbek <pahratdinovdarman@gmail.com>
2025-06-20 23:42:45 +0500

    Update dashboard/default 2.5

--- src/pages/dashboard/default/ui/charts/sales-report.chart.tsx
+++ src/pages/dashboard/default/ui/charts/sales-report.chart.tsx
@@ -2,10 +2,59 @@ import { Card, Checkbox, Flex, Select, Space, Statistic } from "antd"
 import { css, cx } from "antd-style"
 import { type FC } from "react"
 import { useToken } from "src/shared/hooks"
+import { ChartColumn, type ChartColumnConfig } from "src/shared/ui"
 import { formatCountUp } from "src/shared/utils"
+
+const categories = [
+	"Jan",
+	"Feb",
+	"Mar",
+	"Apr",
+	"May",
+	"Jun",
+	"Jul",
+	"Aug",
+	"Sep",
+	"Oct",
+	"Nov",
+	"Dec",
+]
+
+const data = [
+	{
+		category: "Income",
+		data: [180, 90, 135, 114, 120, 145, 170, 200, 170, 230, 210, 180],
+	},
+	{
+		category: "Cost of Sales",
+		data: [120, 45, 78, 150, 168, 99, 180, 220, 180, 210, 220, 200],
+	},
+]
 
 const SalesReportChart: FC = () => {
 	const { token } = useToken()
+
+	const config: ChartColumnConfig = {
+		data: data.flatMap((item) =>
+			categories.map((el, index) => ({
+				month: el,
+				value: item.data[index],
+				category: item.category,
+			}))
+		),
+		xField: "month",
+		yField: "value",
+		legend: false,
+		seriesField: "category",
+		colorField: "category",
+		height: 380,
+		style: {
+			radiusTopLeft: token.borderRadius,
+			radiusTopRight: token.borderRadius,
+			radiusBottomLeft: token.borderRadius,
+			radiusBottomRight: token.borderRadius,
+		},
+	}
 
 	return (
 		<>
@@ -59,6 +108,7 @@ const SalesReportChart: FC = () => {
 						<Checkbox>Cost of Sales</Checkbox>
 					</Space>
 				</Flex>
+				<ChartColumn {...config} />
 			</Card>
 		</>
 	)
